62946480d5b99bf572e8544441fa5287675e3d2e
Amit Sharma <ksamit1110@gmail.com>
2025-08-30 12:46:12 +0530

    feat: dev environment setup

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const ENVIRONMENT_TYPE: 'production' | 'development' = 'production'
+export const ENVIRONMENT_TYPE: 'production' | 'development' = 'development'
 
 const ENVIRONMENTS = {
   production: {
@@ -24,5 +24,5 @@ export const AuthAxios = axios.create({
 })
 
 export const VideoAxios = axios.create({
-  baseURL: ENVIRONMENTS[ENVIRONMENT_TYPE].video+API_VERSION.V1,
+  baseURL: ENVIRONMENTS[ENVIRONMENT_TYPE].video+API_VERSION.V2,
 })
